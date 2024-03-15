2a5a28ea0387a1320b15ab3a243b426813b0e84d
Rudani Mayank <mayankrudani123@gmail.com>
2024-03-15 11:33:52 +0530

    make navbar fully responsive

--- src/modules/home/component/product/index.tsx
+++ src/modules/home/component/product/index.tsx
@@ -4,7 +4,7 @@ const ShopNowProduct = () => {
         <div className="relative bg-[#F1F1F1] py-10 px-5 lg:px-40">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-44">
                 <div>
-                    <div className="text-6xl font-normal my-8">Collections</div>
+                    <div className="text-4xl lg:text-6xl font-normal my-8">Collections</div>
                     <div className="capitalize text-lg my-8 leading-10">
                         you can explore ans shop many differnt collection
                         from various barands here.
@@ -13,11 +13,11 @@ const ShopNowProduct = () => {
                         Shop Now
                     </button>
                 </div>
-                <div className="w-72 relative mt-10 lg:mt-0">
+                <div className="w-44 lg:w-72 relative mt-10 lg:mt-0">
                     <div className="relative z-10">
                         <img className="rounded-tl-[100px] rounded-br-[100px]" src="/engin-akyurt-jaZoffxg1yc-unsplash.png" alt="Product Image" />
                     </div>
-                    <div className="w-72 h-full rounded-tl-[100px] rounded-br-[100px] border-2 border-[#AFAFAF] absolute top-2 -right-2 z-[0]"></div>
+                    <div className="w-44 lg:w-72 h-full rounded-tl-[100px] rounded-br-[100px] border-2 border-[#AFAFAF] absolute top-2 -right-2 z-[0]"></div>
                 </div>
             </div>
             <div className="absolute top-24 right-0 w-44 hidden lg:block">
